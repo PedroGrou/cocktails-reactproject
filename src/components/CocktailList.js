facbddca58e3074d34da07c7ac352abec6f227df
PedroGrou <pedrogroug@gmail.com>
2021-07-19 10:23:41 +0100

    first commit

---
+++ src/components/CocktailList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Cocktail from './Cocktail';
+import Loading from './Loading';
+import { useGlobalContext } from '../context';
+
+const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext();
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
+    return <h2 className="section-title">No cocktails match your search</h2>;
+  }
+
+  return (
+    <section className="section">
+      <h2 className="section-title">cocktail</h2>
+      <div className="cocktails-center">
+        {cocktails.map((drink, id) => {
+          return <Cocktail key={drink.id} {...drink} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default CocktailList;
---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda quod optio unde! Adipisci laborum illo
+        cupiditate illum doloribus veritatis reiciendis ullam, suscipit repudiandae mollitia amet culpa fugiat
+        distinctio harum! Aliquid unde pariatur dicta commodi, temporibus eos quisquam excepturi magni consequuntur.
+      </p>
+    </section>
+  );
+};
+
+export default About;
